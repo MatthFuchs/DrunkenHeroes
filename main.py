8286fdaad4154a8cbe9f2ae5fee306742fd630e1
FlityMcFly <84476614+MatthFuchs@users.noreply.github.com>
2021-12-21 22:28:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import random
 
 from kivy.clock import Clock
@@ -15,6 +14,7 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.uix.gridlayout import GridLayout
+
 Window.size = (800, 500)
 
 Builder.load_string("""
@@ -22,7 +22,7 @@ Builder.load_string("""
     users: username.text
     BoxLayout:
         orientation:'vertical'
-             
+
         TextInput
             id: username
             color_mode: 'custom'
@@ -91,16 +91,17 @@ Builder.load_string("""
             on_release:
                 root.manager.current='menu'
         Button:
-            text:'Bauer'
+            text:'Zur Stadt'
             on_release:
-                root.choose_bauer()
-                root.manager.current='herohub'
-    
-      
+                root.manager.current='Stadt'
+
+
 <Heldenhub>:
+        
+        
     on_pre_enter:
         root.pre()
-        
+
     GridLayout:
         rows:4
         cols:1
@@ -116,8 +117,9 @@ Builder.load_string("""
         MDLabel:
             id:heldbild
             text:'Hier ist ein schönes Bild'
+            font_size:'30sp'
             size_hint_y:0.35
-            
+
         GridLayout:
             size_hint_y:0.25
             rows:1
@@ -125,6 +127,8 @@ Builder.load_string("""
             MDLabel:
                 id:heldleben
                 text:'Leben: 30'
+                font_size:'30sp'
+                color:1,1,1,1
             GridLayout:
                 rows:1
                 cols:2
@@ -143,26 +147,55 @@ Builder.load_string("""
             Button:
                 id:skill1
                 text: 'Skill 1'
+                font_size:'30sp'
                 on_release:
                     root.skillwahl()
                     root.manager.current='menu'
             Button:
                 id:skill2
                 text: 'Skill 2'
+                font_size:'30sp'
                 on_release:
                     root.skillwahl()
-            
+                    
+<Stadt>:
+    canvas:
+        Rectangle:
+            source: 'hintergrund.png'
+            pos: self.pos
+            size: self.size
+    FloatLayout:
+        Button:
+            id:magiershop
+            background_normal: ""
+            pos_hint:{'center_x': 0.4, 'center_y': 0.28}
+            size_hint:0.157,0.25
+            background_color: 0,0,0,0.2
+            on_release:
+                root.magiershop()
+        
+        Button:
+            id:hexeshop
+            background_normal: ""
+            pos_hint:{'center_x': 0.259, 'center_y': 0.659}
+            size_hint:0.15,0.2
+            background_color: 0,0,0,0.2
+            on_release:
+                root.hexeshop()
+    
+      
+
 """)
+
 
 # Declare both screens
 
 
-
 class Startbildschirm(Screen):
-    Helden=["Lanzelot","Dunkler Lord","Prinzessin","Ritter","Hrinloser","Hexe","Zauberer","Braumeister"]
+    Helden = ["Lanzelot", "Dunkler Lord", "Prinzessin", "Ritter", "Hrinloser", "Hexe", "Zauberer", "Braumeister"]
     Heldena = []
     users = StringProperty()
-    heldeninit=True
+    heldeninit = True
 
     def Heldenwahl(self):
         if self.heldeninit == True:
@@ -171,7 +204,7 @@ class Startbildschirm(Screen):
         try:
             Heldenwahl = random.choice(Startbildschirm.Heldena)
             self.popupheld = MDDialog(title="[b][size=30sp][color=ffffff]Held[/color][/size][/b]",
-                                   text="[size=25sp]"+Heldenwahl+"[/size]", auto_dismiss=False, buttons=[
+                                      text="[size=25sp]" + Heldenwahl + "[/size]", auto_dismiss=False, buttons=[
                     MDFlatButton(text='Okay!', on_release=lambda _: self.popupheld.dismiss(), font_size=30)])
             self.popupheld.open()
             print(Heldenwahl)
@@ -183,24 +216,23 @@ class Startbildschirm(Screen):
             self.popupheld.open()
             print("Keine Helden mehr")
 
-
     def Heldenliste(self):
         try:
-            playercount=int(self.users)
+            playercount = int(self.users)
         except:
-            playercount=1
+            playercount = 1
         if playercount == 1:
-            Startbildschirm.Heldena=["Matze"]
+            Startbildschirm.Heldena = ["Matze"]
         elif playercount == 2:
-            Startbildschirm.Heldena=self.Helden[0:2]
+            Startbildschirm.Heldena = self.Helden[0:2]
         elif playercount == 3:
-            Startbildschirm.Heldena= self.Helden[0:3]
+            Startbildschirm.Heldena = self.Helden[0:3]
         elif playercount == 4:
-            Startbildschirm.Heldena = [self.Helden[0],self.Helden[1],self.Helden[3],self.Helden[4]]
+            Startbildschirm.Heldena = [self.Helden[0], self.Helden[1], self.Helden[3], self.Helden[4]]
         elif playercount == 5:
             Startbildschirm.Heldena = self.Helden[0:5]
         elif playercount == 6:
-            Startbildschirm.Heldena=Startbildschirm.Helden
+            Startbildschirm.Heldena = Startbildschirm.Helden
             Startbildschirm.Heldena.remove("Prinzessin")
             Startbildschirm.Heldena.remove("Braumeister")
         elif playercount == 7:
@@ -214,108 +246,131 @@ class Startbildschirm(Screen):
 
 class Heroscreen(Screen):
     global charakter
-    charakter=''
+    charakter = ''
+
     def choose_bauer(self):
-        self.charakter="Bauer"
-        Heldenhub.choosehelden(self,self.charakter)
+        self.charakter = "Bauer"
+        Heldenhub.choosehelden(self, self.charakter)
+
     def choose_lanzelot(self):
-        self.charakter='Lanzelot'
-        Heldenhub.choosehelden(self,self.charakter)
+        self.charakter = 'Lanzelot'
+        Heldenhub.choosehelden(self, self.charakter)
+
     def choose_dunklerlord(self):
-        self.charakter='Dunkler Lord'
-        Heldenhub.choosehelden(self,self.charakter)
+        self.charakter = 'Dunkler Lord'
+        Heldenhub.choosehelden(self, self.charakter)
+
     def choose_ritter(self):
-        self.charakter='Ritter'
-        Heldenhub.choosehelden(self,self.charakter)
+        self.charakter = 'Ritter'
+        Heldenhub.choosehelden(self, self.charakter)
+
     def choose_hirnloser(self):
-        self.charakter='Hirnloser'
-        Heldenhub.choosehelden(self,self.charakter)
+        self.charakter = 'Hirnloser'
+        Heldenhub.choosehelden(self, self.charakter)
+
     def choose_prinzessin(self):
-        self.charakter='Prinzessin'
-        Heldenhub.choosehelden(self,self.charakter)
+        self.charakter = 'Prinzessin'
+        Heldenhub.choosehelden(self, self.charakter)
+
     def choose_hexe(self):
-        self.charakter='Hexe'
-        Heldenhub.choosehelden(self,self.charakter)
+        self.charakter = 'Hexe'
+        Heldenhub.choosehelden(self, self.charakter)
+
     def choose_zauberer(self):
-        self.charakter='Zauberer'
-        Heldenhub.choosehelden(self,self.charakter)
+        self.charakter = 'Zauberer'
+        Heldenhub.choosehelden(self, self.charakter)
+
     def choose_braumeister(self):
-        self.charakter='Braumeister'
-        Heldenhub.choosehelden(self,self.charakter)
+        self.charakter = 'Braumeister'
+        Heldenhub.choosehelden(self, self.charakter)
+
 
 class Heldenhub(Screen):
-    held=''
-    def choosehelden(self,auswahl):
-        self.held=auswahl
-        Heldenhub.held=auswahl
-        print("Held: "+self.held)
-        #Heldenhub.add_widget(self,Label(text="Arsch"))
+    held = ''
+
+    def choosehelden(self, auswahl):
+        self.held = auswahl
+        Heldenhub.held = auswahl
+        print("Held: " + self.held)
+        # Heldenhub.add_widget(self,Label(text="Arsch"))
+
     def pre(self):
         print(Heldenhub.held)
-        self.ids['heldenname'].text=Heldenhub.held
+        self.ids['heldenname'].text = Heldenhub.held
 
     def heldeninfo(self):
-        if self.ids['heldenname'].text=='Lanzelot':
-            self.beschreibung="Lanzelot ist ein tollkühner Held, mit breitem grinsen und aufrechter Statur tritt er dem Dunklen Lord entgegen. \nZiel für Lanzelot ist es, den Dunklen Lord zu besiegen und nebenbei noch die Prinzessin zu klären."
+        if self.ids['heldenname'].text == 'Lanzelot':
+            self.beschreibung = "Lanzelot ist ein tollkühner Held, mit breitem grinsen und aufrechter Statur tritt er dem Dunklen Lord entgegen. \nZiel für Lanzelot ist es, den Dunklen Lord zu besiegen und nebenbei noch die Prinzessin zu klären."
         elif self.ids['heldenname'].text == 'Ritter':
-            self.beschreibung="Der Ritter ist ein treuer Mitstreiter von Lanzelot und unterstützt diesen bei seinen Abenteuern. Ziel ist es, auf Lanzelot aufzupassen, damit sich dieser nicht tollkühn ins Unheil manövriert"
+            self.beschreibung = "Der Ritter ist ein treuer Mitstreiter von Lanzelot und unterstützt diesen bei seinen Abenteuern. Ziel ist es, auf Lanzelot aufzupassen, damit sich dieser nicht tollkühn ins Unheil manövriert"
         elif self.ids['heldenname'].text == 'Dunkler Lord':
-            self.beschreibung="Der Dunkle Lord ist ein dreckiger Schuft, dessen Ziel es ist, Lanzelots Abenteuer zu vermiesen, um Ihm den Tag zu versauen. Nebenbei würde der Dunkle Lord auch gern die Prinzessin verführen."
+            self.beschreibung = "Der Dunkle Lord ist ein dreckiger Schuft, dessen Ziel es ist, Lanzelots Abenteuer zu vermiesen, um Ihm den Tag zu versauen. Nebenbei würde der Dunkle Lord auch gern die Prinzessin verführen."
         elif self.ids['heldenname'].text == 'Hirnloser':
-            self.beschreibung="Der Hirnlose wurde vom Dunklen Lord verhext, um diesem zu dienen und ihn in seinem Vorhaben zu unterstützen, Lanzelot zu besiegen"
+            self.beschreibung = "Der Hirnlose wurde vom Dunklen Lord verhext, um diesem zu dienen und ihn in seinem Vorhaben zu unterstützen, Lanzelot zu besiegen"
         elif self.ids['heldenname'].text == 'Prinzessin':
-            self.beschreibung="Die Prinzessin ist die schärfste Schnitte im ganzen Dorf und kann sich nicht recht entscheiden, ob sie den Dunklen Lord oder Lanzelot als angetrauten möchte. \n Ziel ist es, den beiden Wiedersachern sowohl Hinderniss als auch Unterstützung zu sein."
+            self.beschreibung = "Die Prinzessin ist die schärfste Schnitte im ganzen Dorf und kann sich nicht recht entscheiden, ob sie den Dunklen Lord oder Lanzelot als angetrauten möchte. \n Ziel ist es, den beiden Wiedersachern sowohl Hinderniss als auch Unterstützung zu sein."
         elif self.ids['heldenname'].text == 'Hexe':
-            self.beschreibung="Die Hexe war die Frau des Zauberers. Nach einer unschönen Trennung (an der alleine der Zauberer Schuld trug), hat sie nun ein konkurrierendes Geschäft zum Zauberlädchen eröffnet: Das Hexenlädchen! \n Ziel ist es, den Abenteuerern ihre Waren anzudrehen und vor allem dem Zauberer die Kundschaft zu stehlen indem sie billigere Preise anbietet."
+            self.beschreibung = "Die Hexe war die Frau des Zauberers. Nach einer unschönen Trennung (an der alleine der Zauberer Schuld trug), hat sie nun ein konkurrierendes Geschäft zum Zauberlädchen eröffnet: Das Hexenlädchen! \n Ziel ist es, den Abenteuerern ihre Waren anzudrehen und vor allem dem Zauberer die Kundschaft zu stehlen indem sie billigere Preise anbietet."
         elif self.ids['heldenname'].text == 'Zauberer':
-            self.beschreibung="Der Zauberer hat ein Zauberlädchen, welches bereits seit Generationen im Familienbesitz ist. Leider laufen die Geschäfte nicht mehr so gut wie früher, weil seine Exfrau (die Hexe) auf der gegenüberliegenden Straßenseite einen Laden eröffnet hat, nachdem sie die Trennung nicht verkraften konnte (an welcher alleine sie Schuld war).\n Ziel ist es, die eigenen Waren zu verkaufen indem man die Hexe unterbietet."
+            self.beschreibung = "Der Zauberer hat ein Zauberlädchen, welches bereits seit Generationen im Familienbesitz ist. Leider laufen die Geschäfte nicht mehr so gut wie früher, weil seine Exfrau (die Hexe) auf der gegenüberliegenden Straßenseite einen Laden eröffnet hat, nachdem sie die Trennung nicht verkraften konnte (an welcher alleine sie Schuld war).\n Ziel ist es, die eigenen Waren zu verkaufen indem man die Hexe unterbietet."
         elif self.ids['heldenname'].text == 'Braumeister':
-            self.beschreibung="Der Braumeister kümmert sich schon seit Lebzeiten um die flüssigen Bedurfnisse der Abenteuerer in seinem Dorf. Vorausgesetzt er ist gut gelaunt...\n Ziel ist es, den Abenteuerern Getränke zu verkaufen welche verschiedenste Effekte haben können."
+            self.beschreibung = "Der Braumeister kümmert sich schon seit Lebzeiten um die flüssigen Bedurfnisse der Abenteuerer in seinem Dorf. Vorausgesetzt er ist gut gelaunt...\n Ziel ist es, den Abenteuerern Getränke zu verkaufen welche verschiedenste Effekte haben können."
         elif self.ids['heldenname'].text == 'Bauer':
-            self.beschreibung="Der Bauer hatte eigentlich nicht so richtig Lust auf ein Abenteuer zu gehen, aber dafür dass er die Bäuerin für ein paar Stunden nicht sehen muss, macht er fast alles mit."
+            self.beschreibung = "Der Bauer hatte eigentlich nicht so richtig Lust auf ein Abenteuer zu gehen, aber dafür dass er die Bäuerin für ein paar Stunden nicht sehen muss, macht er fast alles mit."
         else:
             print("Fehler")
         self.popupheldinfo = MDDialog(title="[b][size=30sp][color=ffffff]Heldeninfo[/color][/size][/b]",
-                                  text="[size=25sp]"+self.beschreibung+"[/size]", auto_dismiss=False, buttons=[
+                                      text="[size=25sp]" + self.beschreibung + "[/size]", auto_dismiss=False, buttons=[
                 MDFlatButton(text='Okay!', on_release=lambda _: self.popupheldinfo.dismiss(), font_size=30)])
         self.popupheldinfo.open()
 
     """
-    
+
     hier muss ma jetz unterscheiden wer was machen kann z.B. Zauberer und Hexe haben andere Skills als Ritter und dann muss ma halt schauen wie ma des machen
-    
+
     """
+
     def skillwahl(self):
-        if self.ids['skill1'].text=="Skill 1":
+        if self.ids['skill1'].text == "Skill 1":
             pass
-        elif self.ids['skill1'].text=="Skill 2":
+        elif self.ids['skill1'].text == "Skill 2":
             pass
         else:
             Heldenhub.skilluse(self)
 
     def skilluse(self):
         pass
-    #wenn der held unterschiedliche leben haben soll dann musse ma da no was einfallen lassen, weil ichs hier lock
+
+    # wenn der held unterschiedliche leben haben soll dann musse ma da no was einfallen lassen, weil ichs hier lock
     def lebenplus(self):
-        Leben=self.ids['heldleben'].text
-        Lebenneu=Leben.lstrip("Leben: ")
-        self.ids['heldleben'].text="Leben: "+str(int(Lebenneu)+1)
+        Leben = self.ids['heldleben'].text
+        Lebenneu = Leben.lstrip("Leben: ")
+        self.ids['heldleben'].text = "Leben: " + str(int(Lebenneu) + 1)
 
     def lebenminus(self):
         Leben = self.ids['heldleben'].text
         Lebenneu = Leben.lstrip("Leben: ")
         self.ids['heldleben'].text = "Leben: " + str(int(Lebenneu) - 1)
 
+class Stadt(Screen):
+    def magiershop(self):
+        pass
+
+    def hexeshop(self):
+        pass
 
 class TestApp(MDApp):
 
     def build(self):
+        self.theme_cls.theme_style = "Dark"
         sm = ScreenManager()
         sm.add_widget(Startbildschirm(name='menu'))
         sm.add_widget(Heroscreen(name='hero'))
         sm.add_widget(Heldenhub(name='herohub'))
+        sm.add_widget(Stadt(name='Stadt'))
 
         return sm
+
 
 if __name__ == '__main__':
     TestApp().run()
